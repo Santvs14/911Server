d75552d2396ac9c6bd98e5f782f763c1100f7145
Santiago Vasquez <santiagovasquez1402@gmail>
2024-04-04 17:44:29 -0400

    Otros Cambios adicionales

--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -1,22 +1,25 @@
-const mime = require('mime');
+let mimePromise = import('mime');
 
 export const isAudio = (url: string) => url.includes('.wav');
 
-export const GetBufferByBase64 = (options: { base64: string; name: string }) => {
+export const GetBufferByBase64 = async (options: { base64: string; name: string }) => {
   const { base64, name } = options;
 
   const matches = base64.match(/^data:([A-Za-z-+/]+);base64,(.+)$/);
 
-  if (matches?.length !== 3) throw Error('Format base64 invalido');
+  if (!matches || matches.length !== 3) throw new Error('Formato de base64 inválido');
 
   const decodedImg = {
     type: matches[1],
     data: Buffer.from(matches[2], 'base64'),
   };
 
-  const imageBuffer = decodedImg.data;
+  // Espera a que se resuelva la promesa y obtén el módulo mime
+  const mimeModule = await mimePromise;
+  const mime = mimeModule.default;
+
   const extension = mime.getExtension(decodedImg.type);
   const fileName = `${name}.${extension}`;
 
-  return { fileName, buffer: imageBuffer };
+  return { fileName, buffer: decodedImg.data };
 };
